db67b57b0327325cae7118867fc055616ed20ce3
Jacob <jacob@vvimac.lan>
2020-08-28 18:08:16 -0500

    alive function done

--- multi_graph_former/utils/__init__.py
+++ multi_graph_former/utils/__init__.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def alive(self, tensor, threshold, c=2):
+def alive(self, tensor, threshold=0.15, c=2):
     """qualifies the tensor as alive or dead. 
     The formula makes even a one hot encoding considered alive
 
@@ -17,22 +17,3 @@ def alive(self, tensor, threshold, c=2):
     """
     tensor = tf.tanh( c * tf.norm(tensor, axis=-1) / tf.sqrt(tf.shape(tensor)[-1]) )
     return tf.where(tensor > threshold, tensor, tf.zeros_like(tensor))
-
-
-def count_alive(verts, edges):
-    """counts live verts and edges in graph. differentiable
-
-    params:
-        verts: tensor: LEADING_DIMS + (N_verts, d_vert)
-        edges: tensor: LEADING_DIMS + (N_verts, N_verts, d_edge)
-
-    returns:
-        a tuple (num live verts, num live edges)
-    """
-    
-    live_verts = alive(verts, self.VERT_MIN)
-    live_verts = tf.reduce_sum(live_verts, axis=-1)
-    live_edges = alive(edges, self.ADJ_MIN)
-    live_edges = tf.reduce_sum(live_edges, axis=[-2,-1])
-
-    return live_verts, live_edges
